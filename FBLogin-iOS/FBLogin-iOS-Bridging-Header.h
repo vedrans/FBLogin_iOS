5a5e50148436c7eaf070ba91eafc0f547348df19
Vedran Stankovic <contact@vedrans.me>
2016-05-02 18:45:31 +0100

    bridging header

---
+++ FBLogin-iOS/FBLogin-iOS-Bridging-Header.h
@@ -0,0 +1,16 @@
+//
+//  FBLogin-iOS-Bridging-Header.h
+//  FBLogin-iOS
+//
+//  Created by Vedran Stankovic on 02/05/2016.
+//
+//
+
+#ifndef FBLogin_iOS_Bridging_Header_h
+#define FBLogin_iOS_Bridging_Header_h
+
+#import<FBSDKCoreKit/FBSDKCoreKit.h>
+#import<FBSDKLoginKit/FBSDKLoginKit.h>
+
+
+#endif /* FBLogin_iOS_Bridging_Header_h */
